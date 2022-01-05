58ca0210a00ced4ac15210a18d13e25bdf91c593
SnowBit <dtshah24022006@gmail.com>
2022-01-05 16:17:56 +0530

    Add dat.gui

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import db from '../firebase'
 import {useParams, useNavigate} from 'react-router-dom'
+import * as dat from 'dat.gui';
 const tinyid = require('tiny-unique-id')
+
 
 
 function Textarea() {
@@ -23,7 +25,7 @@ function Textarea() {
         today =  dd + '/' + mm + '/' + yyyy
         await db.collection('notes').doc(slug).set({
             text: text,
-            date: today
+            date: today,
         })
         navigate(`/${slug}`)
     }
@@ -42,6 +44,28 @@ function Textarea() {
         }
         handleLoad()
     }, [slug])
+    useEffect(() => {
+        const settings = async () => {
+        const gui = await new dat.GUI({hideable: false});
+        const obj = {
+            fontSize : 25,
+            fontFamily : 'monospace',
+        }
+        gui.add(obj, 'fontSize').min(8).max(60).step(1).onChange(e => {
+            document.querySelector('textarea').style.fontSize = `${e}px`
+        })
+
+        gui.add(obj, 'fontFamily', {
+            'Monospace': 'monospace',
+            'Roboto': 'Roboto',
+            'Poppins': 'Poppins',
+            'Cursive': 'Cursive',
+        }).onChange(e => {
+            document.querySelector('textarea').style.fontFamily = e
+        })
+    }
+    setTimeout(settings, 1500)
+    }, [])
     return (
         <div onKeyDown={Shortcut}>
             <textarea id="txt" cols="30" rows="10" placeholder="Enter some text!" value={text} onChange={e => setText(e.target.value)} onKeyDown={keySound}></textarea>
